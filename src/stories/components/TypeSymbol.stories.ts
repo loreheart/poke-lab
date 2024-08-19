48cafb9b542762c8eed4984f1d4539fa50c74214
Kaia <13533828+Lawlheart@users.noreply.github.com>
2024-08-19 14:14:55 -0700

    adjust stories

--- src/stories/components/TypeSymbol.stories.ts
+++ src/stories/components/TypeSymbol.stories.ts
@@ -8,7 +8,7 @@ const meta = {
   component: TypeSymbol,
   argTypes: {
     pokeTypeName: { control: 'radio', options: pokeTypes.map(({ name }) => name) },
-    size: { control: 'radio', options: ['small', 'medium', 'large'] },
+    size: { control: 'radio', options: ['mini', 'small', 'medium', 'large'] },
     shape: { control: 'radio', options: ['squared', 'circular'] },
     showName: { control: 'boolean', options: [true, false] }
   },
@@ -26,102 +26,12 @@ export const Defaults: Story = {
   },
 }
 
-export const NoTitleMiniRounded: Story = {
+export const MiniPokedex: Story = {
   args: {
     pokeTypeName: pokeTypes[11].name,
     shape: 'circular',
     size: 'mini',
     showName: false,
-  },
-}
-
-export const NoTitleSmall: Story = {
-  args: {
-    pokeTypeName: pokeTypes[1].name,
-    shape: 'squared',
-    size: 'small',
-    showName: false,
-  },
-}
-
-export const NoTitleMedium: Story = {
-  args: {
-    pokeTypeName: pokeTypes[2].name,
-    shape: 'squared',
-    size: 'medium',
-    showName: false,
-  },
-}
-
-export const NoTitleLarge: Story = {
-  args: {
-    pokeTypeName: pokeTypes[3].name,
-    shape: 'squared',
-    size: 'large',
-    showName: false,
-  },
-}
-
-export const NoTitleRoundedSmall: Story = {
-  args: {
-    pokeTypeName: pokeTypes[4].name,
-    shape: 'circular',
-    size: 'small',
-    showName: false,
-  },
-}
-
-export const NoTitleRoundedMedium: Story = {
-  args: {
-    pokeTypeName: pokeTypes[5].name,
-    shape: 'circular',
-    size: 'medium',
-    showName: false,
-  },
-}
-
-export const NoTitleRoundedLarge: Story = {
-  args: {
-    pokeTypeName: pokeTypes[6].name,
-    shape: 'circular',
-    size: 'large',
-    showName: false,
-  },
-}
-
-export const TitleSmallRounded: Story = {
-  args: {
-    pokeTypeName: pokeTypes[7].name,
-    shape: 'circular',
-    size: 'small',
-    showName: true,
-  },
-}
-
-export const TitleMediumRounded: Story = {
-  args: {
-    pokeTypeName: pokeTypes[8].name,
-    shape: 'circular',
-    size: 'medium',
-    showName: true,
-  },
-}
-
-export const TitleLargeRounded: Story = {
-  args: {
-    pokeTypeName: pokeTypes[9].name,
-    shape: 'circular',
-    size: 'large',
-    showName: true,
-  },
-}
-
-export const TitleMiniSquared: Story = {
-  args: {
-    pokeTypeName: pokeTypes[7].name,
-    shape: 'squared',
-    size: 'mini',
-    showName: true,
   },
 }
 
@@ -134,7 +44,7 @@ export const TitleSmallSquared: Story = {
   },
 }
 
-export const TitleMediumSquared: Story = {
+export const PokedexSquared: Story = {
   args: {
     pokeTypeName: pokeTypes[8].name,
     shape: 'squared',
@@ -143,11 +53,12 @@ export const TitleMediumSquared: Story = {
   },
 }
 
-export const TitleLargeSquared: Story = {
+
+export const BigRoundGrass: Story = {
   args: {
-    pokeTypeName: pokeTypes[9].name,
-    shape: 'squared',
+    pokeTypeName: 'grass',
+    shape: 'circular',
     size: 'large',
-    showName: true,
-  },
+    showName: true
+  }
 }
